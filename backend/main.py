aed0e62bdc13770ecac0860a4cc77d3f6cdc37c1
omkhanjodkar <okkhanjodkar@gmail.com>
2025-12-06 13:43:30 +0530

    added create_attendance post request

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.post("/attendance/{roll_number} {date} {time} {subject}")
+def create_attendance(roll_number: int, date: str, time: str, subject: str):
+    return {"message": "Attendance created"}
